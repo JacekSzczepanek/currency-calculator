89a03d69b5d87d96f20edceef413ff0675df29db
Jacek Szczepanek <jacekszczepanek91@gmail.com>
2022-11-15 19:17:57 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let amountElement = document.querySelector(".js-amount");
+let cashElement = document.querySelector(".js-cash");
+let formElement = document.querySelector(".js-form");
+let resultElement = document.querySelector(".js-result");
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let cash = cashElement.value;
+  let amount = amountElement.value;
+
+  let result = amount / cash;
+
+  resultElement.innerText = result.toFixed(2);
+});
