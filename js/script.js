62fe12da65df59d7d34bcd1b7bc3945c935ae9e0
Jacek Szczepanek <jacekszczepanek91@gmail.com>
2022-11-21 13:53:51 +0100

    js edit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-let amountElement = document.querySelector(".js-amount");
-let cashElement = document.querySelector(".js-cash");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
+{
+  let amountElement = document.querySelector(".js-amount");
+  let cashElement = document.querySelector(".js-cash");
+  let formElement = document.querySelector(".js-form");
+  let resultElement = document.querySelector(".js-result");
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  let cash = cashElement.value;
-  let amount = amountElement.value;
+    let cash = cashElement.value;
+    let amount = amountElement.value;
 
-  let result = amount / cash;
+    let result = amount / cash;
 
-  resultElement.innerText = result.toFixed(2);
-});
+    resultElement.innerText = result.toFixed(2);
+  });
+}
